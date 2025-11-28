ebca5d5228c68fb6680e5eb39f4a441b82b8666b
kibe-zeroone <gabrielnunesdemelo770@gmail.com>
2025-11-28 16:37:13 -0300

    corrijindo tamanho da logo

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -19,7 +19,7 @@ export function Navigation({ onLoginClick }: NavigationProps) {
               <img
                 src="/logo.png"
                 alt="CasaFácil Logo"
-                className="h-18 w-auto"
+                className="h-14 w-auto"
               />
             </div>
           </div>
